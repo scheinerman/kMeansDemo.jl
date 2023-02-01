e668a89e64cb9ee5f19d0da0370302a5e2c2a631
Ed Scheinerman <ers@jhu.edu>
2023-02-01 14:55:14 -0500

    startup

---
+++ README.md
@@ -0,0 +1,54 @@
+# `kMeansDemo`
+
+This is a small demonstration project of k-means clustering with k=2 and in two dimensions.
+
+## Point Sets
+
+Points are represented as complex numbers. Input to the method is a list (`Vector`) of points. 
+
+For demonstration purposes, we provide the function `double_cluster` to generate a set of points. 
+
+Usage: `double_cluster(n, offset)` generates a list of `2n` points. The first `n` points are
+Gaussian normal centered around the origin and the second `n` points are Gaussian normal 
+centered around `offset`. 
+
+For example, `double_cluster(1000, 3+3im)`. 
+
+## Single stepping through the k-means algorithm
+
+The function `one_step` is used to perform one step of the `k`-means algorithm. 
+
+If `pts` is the list of points, then `a,b = one_step(pts)` should be the first invocation. 
+This simply partitions `pts` into two lists `a` and `b` by randomly 
+assigning points to `a` or `b`.
+
+Subsequent invocations of `one_step` looks like this `a,b = one_step(a,b)`. If the new partition 
+is the same as the given partition, the message `No change` is displayed.
+
+## Visualization
+
+`draw_list(pts)` plots all points in `list` as blue dots.
+
+`draw_list(a,b)` plots the points in `a` in blue and the points in `b` in red. 
+
+## Example
+
+```
+julia> using kMeansDemo
+
+julia> pts = double_cluster(1000, 5);
+
+julia> a,b = one_step(pts);
+
+julia> a,b = one_step(a,b);
+
+julia> a,b = one_step(a,b);
+
+julia> a,b = one_step(a,b);
+[ Info: No change
+
+julia> draw_list(a,b)
+```
+
+The result looks like this: 
+![](./example.png)
---
+++ src/kMeansDemo.jl
@@ -0,0 +1,109 @@
+module kMeansDemo
+using Plots, SimpleDrawing
+
+export draw_list, one_step, double_cluster
+
+# export rand_split_list, one_step, nearer_to
+
+"""
+    nearer_to(x::Complex, a::Complex, b::Complex)::Int
+
+Return `1` is `x` is closer to `a` or `2` is closer to `b`.
+"""
+function nearer_to(x::Complex, a::Complex, b::Complex)
+    da = abs(x - a)
+    db = abs(x - b)
+    return da < db ? 1 : 2
+end
+
+"""
+    split_list(a::Complex, b::Complex, list::Vector{Complex})
+
+Partition the values in `list` based on whether they are closer to `a`
+or to `b`. Returns two lists, the first being those closer to `a` and 
+the second being those closer to `b`.
+"""
+function split_list(a::Complex{T}, b::Complex{S}, list::Vector) where {S,T}
+    n = length(list)
+    a_list = [z for z in list if nearer_to(z, a, b) == 1]
+    b_list = [z for z in list if nearer_to(z, a, b) == 2]
+    return a_list, b_list
+end
+
+
+function rand_split_list(list::Vector{Complex{T}}) where {T}
+    n = length(list)
+    idx = [mod(rand(Int), 2) for _ = 1:n]
+
+    a = [list[k] for k = 1:n if idx[k] == 0]
+    b = [list[k] for k = 1:n if idx[k] == 1]
+    return a, b
+end
+
+avg(list::Vector) = sum(list) / length(list)
+
+
+function one_step(alist::Vector, blist::Vector)
+    a = avg(alist)
+    b = avg(blist)
+
+    aa, bb = split_list(a, b, [alist; blist])
+
+    if aa == alist && bb == blist
+        @info "No change"
+    end
+
+    return aa, bb
+end
+
+one_step(list::Vector) = rand_split_list(list)
+
+
+function draw_one(z::Complex, the_color = :red)
+    draw_point(z, marker = 2, markerstrokecolor = the_color, markercolor = the_color)
+end
+
+"""
+    draw_list(zlist::Vector, the_color = :red)
+
+Draw the points in `zlist` in the color `the_color`.
+"""
+function _draw_list(zlist::Vector, the_color = :red)
+    for z in zlist
+        draw_one(z, the_color)
+    end
+end
+
+"""
+    draw_list(alist::Vector, blist::Vector)
+    draw_list(alist::Vector)
+
+Plot one or two lists of points. First list is blue. Second 
+list is red. 
+"""
+function draw_list(alist::Vector, blist::Vector)
+    newdraw()
+    _draw_list(alist, :blue)
+    _draw_list(blist, :red)
+    finish()
+end
+
+
+function draw_list(alist::Vector)
+    newdraw()
+    _draw_list(alist,:blue)
+    finish()
+end
+
+"""
+    double_cluster(n::Int=1000, z::Number = 5)::Vector{Complex{Float64}}
+
+Create `2n` points with `n` chosen Gaussian around 0 and `n` chosen Gaussian around `z`. 
+"""
+function double_cluster(n::Int=1000, z::Number = 5)::Vector{Complex{Float64}}
+    p1 = randn(ComplexF64,n)
+    p2 = randn(ComplexF64,n) .+ z
+    return [p1;p2]
+end
+
+end # module kMeansDemo
